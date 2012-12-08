fd4992902401357338ecd076136617c9f980bbb4
phoenixg-compaq321 <306761352@qq.com>
2012-12-08 20:22:13 +0800

    if lucky, shutter will work in test controller

--- application/views/test.blade.php
+++ application/views/test.blade.php
@@ -6,15 +6,36 @@ PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
 	<title></title>
 
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />	
-	{{ HTML::script('js/jquery-1.8.0.min.js') }}
-	<!--
-	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
-	-->
+	
+	{{ HTML::style('css/profile.css') }} <!-- <link rel="stylesheet" type="text/css" href="assets/css/styles.css" /> -->
+	{{ HTML::style('css/jquery.shutter.css') }} <!-- <link rel="stylesheet" type="text/css" href="assets/jquery.shutter/jquery.shutter.css" /> -->
+
+
+
+
+	
+	
+	
+
 </head>
 
 <body>
 
 <h1>Boy you are in test</h1>
+
+<div id="container">
+<ul>
+        <li><img src="http://173.230.150.168/gitprojects/seekfortruelove/public/images/profile/icon/2.jpg" width="140" height="300" alt="Landscape" /></li>
+        <li><img src="http://cdn.tutorialzine.com/wp-content/uploads/2011/03/shutter-effect-canvas-jquery.jpg" width="640" height="400" alt="Yacht club" /></li>
+        <li><img src="assets/img/photos/3.jpg" width="640" height="400" alt="Desert" /></li>
+    </ul>
+</div>
+
+
+{{ HTML::script('js/jquery-1.8.0.min.js') }}
+{{ HTML::script('js/jquery.shutter.js') }}
+{{ HTML::script('js/profile.js') }}
+
 
 
 									
