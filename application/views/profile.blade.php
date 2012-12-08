ab96e690e262868b69e2d36addd93ff8d468ba35
phoenixg-compaq321 <306761352@qq.com>
2012-12-08 21:44:14 +0800

    profile

--- application/views/profile.blade.php
+++ application/views/profile.blade.php
@@ -1,6 +1,7 @@
 @layout('layouts.default')
 @section('page_styles_header')
 	{{ HTML::style('css/profile.css') }}
+	{{ HTML::style('css/search.css') }}
 	{{ HTML::style('css/jquery....css') }}
 @endsection
 @section('page_scripts_header')
@@ -17,6 +18,13 @@
 	<br />
 
 	<ul id="thumbnails" class="thumbnails">
+		<li>
+			<a href="" class="fancybox" rel="external">
+			<span class="imghover_gallery"></span>
+			<img src="http://173.230.150.168/gitprojects/seekfortruelove/public/images/profile/icon/2.jpg" />
+			</a>
+		</li>
+
 		<li>
 			<a href="" class="fancybox" rel="external">
 			<span class="imghover_gallery"></span>
