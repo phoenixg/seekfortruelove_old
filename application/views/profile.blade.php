4edc05365ef97b2bc97cb9ad4cd22bd5f88ac451
phoenixg-compaq321 <306761352@qq.com>
2012-12-08 21:40:38 +0800

    在profile里用黑白图怎样？

--- application/views/profile.blade.php
+++ application/views/profile.blade.php
@@ -16,6 +16,13 @@
 
 	<br />
 
-
+	<ul id="thumbnails" class="thumbnails">
+		<li>
+			<a href="" class="fancybox" rel="external">
+			<span class="imghover_gallery"></span>
+			<img src="http://173.230.150.168/gitprojects/seekfortruelove/public/images/profile/icon/2.jpg" />
+			</a>
+		</li>
+	</ul>
 </div>
 @endsection
