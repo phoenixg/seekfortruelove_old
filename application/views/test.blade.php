d79c90c51e90e2497912434904563555c083c528
phoenixg-compaq321 <306761352@qq.com>
2012-12-08 21:00:50 +0800

    i don't believe love any more

--- application/views/test.blade.php
+++ application/views/test.blade.php
@@ -6,9 +6,57 @@ PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
 	<title></title>
 
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />	
+<!--
 	{{ HTML::style('css/profile.css') }} 
 	{{ HTML::script('js/jquery.shutter/jquery.shutter.css') }} 
-	 
+-->
+<style type="text/css">
+*{
+margin:0;
+padding:0;
+}
+
+
+#container{
+width:640px;
+height:400px;
+width:240px;
+height:240px;
+
+
+margin:0 auto;
+border:5px solid #fff;
+overflow:hidden;
+-moz-box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
+-webkit-box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
+box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
+}
+
+#container ul{
+list-style:none;
+}
+
+
+.shutterAnimationHolder .film canvas{
+display: block;
+    margin: 0 auto;
+}
+
+.shutterAnimationHolder .film{
+position:absolute;
+left:50%;
+top:0;
+}
+
+.shutterAnimationHolder{
+position:absolute;
+overflow:hidden;
+top:0;
+left:0;
+z-index:1000;
+}
+</style>
+
 </head>
 
 <body>
@@ -19,7 +67,6 @@ PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
 <ul>
         <li><img src="http://173.230.150.168/gitprojects/seekfortruelove/public/images/profile/icon/2.jpg" width="140" height="300" alt="Landscape" /></li>
         <li><img src="http://cdn.tutorialzine.com/wp-content/uploads/2011/03/shutter-effect-canvas-jquery.jpg" width="640" height="400" alt="Yacht club" /></li>
-        <!--<li><img src="assets/img/photos/3.jpg" width="640" height="400" alt="Desert" /></li>-->
     </ul>
 </div>
 
@@ -27,6 +74,6 @@ PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
 {{ HTML::script('js/jquery-1.8.0.min.js') }}
 
 {{ HTML::script('js/jquery.shutter/jquery.shutter.js') }}
-{{ HTML::script('js/profile.js') }}								
+{{ HTML::script('js/profile.js') }}
 </body>
 </html>
