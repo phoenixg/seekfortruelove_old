ac65f79d548af25d7712bcba312f9dece2b31416
phoenix <306761352@qq.com>
2012-12-12 14:00:05 +0800

    邮件测试

--- application/controllers/account.php
+++ application/controllers/account.php
@@ -53,10 +53,10 @@ class Account_Controller extends Base_Controller
 $mailer = Laravel\IoC::resolve('mailer');
 
 $messageBody = 
-'您好'.Input::get('nickname').
+'您好'.Input::get('nickname')."\n"
 '弟兄/姐妹，这封信是由seekfortruelove网站自动发出的，您收到这封邮件，是由于在[seekfortruelove网址]进行了新用户注册。您的用户名是：'.Input::get('email').'
 
-如果您并没有访问过seekfortruelove网站，请忽略这封邮件。
+如果您并没有访问过seekfortruelove网站，请忽略这封邮件。<br />
 
 您只需点击下面的链接即可激活您的帐号：
 http://www.domain.com/code123456789
