48e1808b27b2cdde128d2521fcb4f0c66808b790
phoenix <306761352@qq.com>
2012-12-17 15:29:25 +0800

    111

--- application/controllers/account.php
+++ application/controllers/account.php
@@ -168,8 +168,10 @@ class Account_Controller extends Base_Controller
 				exit;
 			}
 
-			// 如果是其他情况，就返回404页面
-			return Response::error('404');
+			// 如果是其他情况
+			return Redirect::to_route('verifymail')
+				->with('msg_verify_error_wrong', '');
+			exit;
 		}
 	}
 
