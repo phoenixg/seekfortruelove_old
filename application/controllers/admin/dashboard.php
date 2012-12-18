508e543e0551aaac3c9867e78c100a1b7ca039fb
phoenixg-compaq321 <306761352@qq.com>
2012-12-18 20:43:48 +0800

    11

--- application/controllers/admin/dashboard.php
+++ application/controllers/admin/dashboard.php
@@ -3,6 +3,12 @@ class Admin_Dashboard_Controller extends Base_Controller
 {
 	public $restful = true;
 
+	/* todo
+	public function __construct() {
+		$this->filter('before', 'auth');
+	}
+	*/
+
 	public function get_index()
 	{
 		return View::make('dashboard.index')
