7ad13a10504fbebbe104fc413edb0f567ac88475
phoenix <306761352@qq.com>
2012-12-19 14:35:31 +0800

    111

--- application/controllers/admin/admin.php
+++ application/controllers/admin/admin.php
@@ -3,13 +3,12 @@ class Admin_Admin_Controller extends Base_Controller
 {
 	public $restful = true;	
 
-	public function get_index()
-	{
-	}
-
-
 	public function get_login()
 	{
+		if (Auth::check()) {
+			return 'you are logged in';
+		}
+
 		return View::make('admin.login');
 	}
 
