0db12e8c78edc6949247072f39e643b3ed5041dd
phoenix <306761352@qq.com>
2012-12-19 16:07:39 +0800

    发飞机啊

--- application/views/admin/panelindex.blade.php
+++ application/views/admin/panelindex.blade.php
@@ -1,1 +1,15 @@
-panelindex
+@layout('admin.panel')
+@section('page_styles_header')
+	{{ HTML::style('css/...css') }}
+@endsection
+@section('page_scripts_header')
+	{{ HTML::script('js/...js') }}
+@endsection
+@section('page_scripts_footer')
+	{{ HTML::script('js/...js') }}
+@endsection
+@section('main')
+
+	panelindex
+
+@endsection
