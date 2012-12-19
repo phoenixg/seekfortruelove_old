5b2e1b3640a068922e93cb5f0d58c64dcda1f74b
phoenix <306761352@qq.com>
2012-12-19 15:42:40 +0800

    发而发

--- application/controllers/admin/admin.php
+++ application/controllers/admin/admin.php
@@ -12,14 +12,12 @@ class Admin_Admin_Controller extends Base_Controller
 
 	public function post_login()
 	{
-		var_dump(Input::get());
+		$admin = DB::table('admins')->first();
 
-		echo '<pre>';print_r(DB::table('admins')->first());
-die;
-		if(Hash::check(trim(Input::get('password')), ''))
-		{
-			//return Redirect::to_route('search');
-			return Redirect::to('search');
+		if(Hash::check(trim(Input::get('password')), $admin->password))
+		{	
+
+			return Redirect::to_route('search');
 			exit;
 		}
 		
