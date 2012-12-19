0ce78e7970cc2cbe3ca5f905edb3653bc3835eea
phoenix <306761352@qq.com>
2012-12-19 13:55:38 +0800

    删除jquery_news_ticker

--- application/views/test.blade.php
+++ application/views/test.blade.php
@@ -6,58 +6,14 @@ PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
 	<title></title>
 
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />	
-	<link href="http://173.230.150.168/gitprojects/seekfortruelove/jquery_news_ticker/styles/ticker-style.css" rel="stylesheet" type="text/css" />
-  <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.0/jquery.min.js"></script> 
-	<script src="http://173.230.150.168/gitprojects/seekfortruelove/jquery_news_ticker/includes/jquery.ticker.js" type="text/javascript"></script>
 
-<style type="text/css">
+  <style type="text/css">
 
-
-ul, li {
-  margin:0;
-  padding:0;
-  border:0;
-  outline:0;
-  font-size:100%;
-  vertical-align:baseline;
-  background:transparent;
-}                  
-
-
-</style>
-
-
-
-
-
-
-
-
-
-
-
-
-
+  </style>
 
 </head>
 
 <body>
-
-<ul id="js-news" class="js-hidden">
-    <li class="news-item"><a href="#">This is the 1st latest news item.</a></li>
-    <li class="news-item"><a href="#">This is the 2nd latest news item.</a></li>
-    <li class="news-item"><a href="#">This is the 3rd latest news item.</a></li>
-    <li class="news-item"><a href="#">This is the 4th latest news item.</a></li>
-</ul>
-<script type="text/javascript">
-$(function () {
-     // start the ticker 
-    $('#js-news').ticker();
-    
-
-});         
-</script>
-
 
 
 <h1>Boy you are in test, what can I do for you?</h1>
--- jquery_news_ticker/includes/site.js
+++
@@ -1,7 +0,0 @@
-$(function () {
-     // start the ticker 
-	$('#js-news').ticker();
-	
-
-});
-
