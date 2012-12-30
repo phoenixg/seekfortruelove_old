df844c1db4d9ec19d60f58065d9a7d2f32f4f274
phoenixg-compaq321 <306761352@qq.com>
2012-12-30 23:28:00 +0800

    添加 facebook share按钮

--- application/views/home/index.blade.php
+++ application/views/home/index.blade.php
@@ -17,6 +17,19 @@
 				<!-- twitter share button -->
 				<a href="https://twitter.com/share" class="twitter-share-button" data-lang="zh-cn">发推</a>
 				<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+
+
+				<!-- facebook share button -->
+				<div id="fb-root"></div>
+				<script>(function(d, s, id) {
+				  var js, fjs = d.getElementsByTagName(s)[0];
+				  if (d.getElementById(id)) return;
+				  js = d.createElement(s); js.id = id;
+				  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
+				  fjs.parentNode.insertBefore(js, fjs);
+				}(document, 'script', 'facebook-jssdk'));</script>
+				<div class="fb-like" data-href="http://seekfortruelove.org" data-send="false" data-layout="button_count" data-width="450" data-show-faces="false" data-action="recommend"></div>
+
 			</p>
 		</div>
 
