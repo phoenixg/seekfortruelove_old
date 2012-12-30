3b6191fb91ab316eb253770b6a5b76f932d3dcf8
phoenixg-compaq321 <306761352@qq.com>
2012-12-30 22:19:40 +0800

    12

--- application/views/site/memorabilia.blade.php
+++ application/views/site/memorabilia.blade.php
@@ -5,10 +5,13 @@
 	<h1>网站大事记</h1>
 	<p>2012年12月21日 上线</p>
 
-	<p>2012年12月22日 第一位注册用户诞生</p>
-	<img src="{{ URL::base() . '/images/memorabilia/' . 'screenshot20121229.png' }}" alt="" />
+	<p>2012年12月22日 第1位注册用户诞生</p>
 
 	<p>2012年12月26日 在yimaneili和赞美社先后遭到删贴，不得不改成非主内定位</p>
-	<img src="{{ URL::base() . '/images/memorabilia/' . 'screenshot20121230.png' }}" alt="" />
+
+	<a href="{{ URL::base() . '/images/memorabilia/' . 'screenshot20121229.png' }}">screenshot</a>
+
+	<p>2012年12月30日的截图</p>
+	<a href="{{ URL::base() . '/images/memorabilia/' . 'screenshot20121230.png' }}">screenshot</a>
 </div>
 @endsection
