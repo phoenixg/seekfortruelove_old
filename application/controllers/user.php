2652956e59abbf054846b837e6ed13be8b678830
phoenix <306761352@qq.com>
2013-01-02 16:33:52 +0800

    112

--- application/controllers/user.php
+++ application/controllers/user.php
@@ -2,156 +2,156 @@
 
 class User_Controller extends Base_Controller
 {
-	public $restful = true;
+    public $restful = true;
 
-	public function get_search() {
-		return View::make('search');
-			//->with('users', User::all());
-	}
+    public function get_search() {
+        return View::make('search');
+            //->with('users', User::all());
+    }
 
-	public function post_search() {	
-		
-		$qHeightSmaller = (int) Input::get('qHeightSmaller');
-		$qHeightTaller  = (int) Input::get('qHeightTaller');
+    public function post_search() { 
+        
+        $qHeightSmaller = (int) Input::get('qHeightSmaller');
+        $qHeightTaller  = (int) Input::get('qHeightTaller');
 
-		$qSalaryLow     = (int) Input::get('qSalaryLow');
-		$qSalaryHigh    = (int) Input::get('qSalaryHigh');
-		if ($qSalaryLow < 1000) {
-			$qSalaryLow = 1;
-		} elseif ($qSalaryLow < 1900) {
-			$qSalaryLow = 2;
-		} elseif ($qSalaryLow < 2900) {
-			$qSalaryLow = 3;
-		} elseif ($qSalaryLow < 3900) {
-			$qSalaryLow = 4;
-		} elseif ($qSalaryLow < 4900) {
-			$qSalaryLow = 5;
-		} elseif ($qSalaryLow < 5900) {
-			$qSalaryLow = 6;
-		} elseif ($qSalaryLow < 6900) {
-			$qSalaryLow = 7;
-		} elseif ($qSalaryLow < 7900) {
-			$qSalaryLow = 8;
-		} elseif ($qSalaryLow < 8900) {
-			$qSalaryLow = 9;
-		} elseif ($qSalaryLow < 9900) {
-			$qSalaryLow = 10;
-		} elseif ($qSalaryLow < 11900) {
-			$qSalaryLow = 11;
-		} elseif ($qSalaryLow < 14900) {
-			$qSalaryLow = 12;
-		} elseif ($qSalaryLow < 19900) {
-			$qSalaryLow = 13;
-		} elseif ($qSalaryLow < 29900) {
-			$qSalaryLow = 14;
-		} elseif ($qSalaryLow < 30000) {
-			$qSalaryLow = 15;
-		}
-		
-		if ($qSalaryHigh < 1000) {
-			$qSalaryHigh = 1;
-		} elseif ($qSalaryHigh < 1900) {
-			$qSalaryHigh = 2;
-		} elseif ($qSalaryHigh < 2900) {
-			$qSalaryHigh = 3;
-		} elseif ($qSalaryHigh < 3900) {
-			$qSalaryHigh = 4;
-		} elseif ($qSalaryHigh < 4900) {
-			$qSalaryHigh = 5;
-		} elseif ($qSalaryHigh < 5900) {
-			$qSalaryHigh = 6;
-		} elseif ($qSalaryHigh < 6900) {
-			$qSalaryHigh = 7;
-		} elseif ($qSalaryHigh < 7900) {
-			$qSalaryHigh = 8;
-		} elseif ($qSalaryHigh < 8900) {
-			$qSalaryHigh = 9;
-		} elseif ($qSalaryHigh < 9900) {
-			$qSalaryHigh = 10;
-		} elseif ($qSalaryHigh < 11900) {
-			$qSalaryHigh = 11;
-		} elseif ($qSalaryHigh < 14900) {
-			$qSalaryHigh = 12;
-		} elseif ($qSalaryHigh < 19900) {
-			$qSalaryHigh = 13;
-		} elseif ($qSalaryHigh < 29900) {
-			$qSalaryHigh = 14;
-		} elseif ($qSalaryHigh <= 30000) {
-			$qSalaryHigh = 15;
-		}	
-		
-		$qBornYoung        = date('Y') - (int) Input::get('qAgeYoung');
-		$qBornOld          = date('Y') - (int) Input::get('qAgeOld');
-		
-		$qAcademicLow      = (int) Input::get('qAcademicLow');
-		$qAcademicHigh     = (int) Input::get('qAcademicHigh');
+        $qSalaryLow     = (int) Input::get('qSalaryLow');
+        $qSalaryHigh    = (int) Input::get('qSalaryHigh');
+        if ($qSalaryLow < 1000) {
+            $qSalaryLow = 1;
+        } elseif ($qSalaryLow < 1900) {
+            $qSalaryLow = 2;
+        } elseif ($qSalaryLow < 2900) {
+            $qSalaryLow = 3;
+        } elseif ($qSalaryLow < 3900) {
+            $qSalaryLow = 4;
+        } elseif ($qSalaryLow < 4900) {
+            $qSalaryLow = 5;
+        } elseif ($qSalaryLow < 5900) {
+            $qSalaryLow = 6;
+        } elseif ($qSalaryLow < 6900) {
+            $qSalaryLow = 7;
+        } elseif ($qSalaryLow < 7900) {
+            $qSalaryLow = 8;
+        } elseif ($qSalaryLow < 8900) {
+            $qSalaryLow = 9;
+        } elseif ($qSalaryLow < 9900) {
+            $qSalaryLow = 10;
+        } elseif ($qSalaryLow < 11900) {
+            $qSalaryLow = 11;
+        } elseif ($qSalaryLow < 14900) {
+            $qSalaryLow = 12;
+        } elseif ($qSalaryLow < 19900) {
+            $qSalaryLow = 13;
+        } elseif ($qSalaryLow < 29900) {
+            $qSalaryLow = 14;
+        } elseif ($qSalaryLow < 30000) {
+            $qSalaryLow = 15;
+        }
+        
+        if ($qSalaryHigh < 1000) {
+            $qSalaryHigh = 1;
+        } elseif ($qSalaryHigh < 1900) {
+            $qSalaryHigh = 2;
+        } elseif ($qSalaryHigh < 2900) {
+            $qSalaryHigh = 3;
+        } elseif ($qSalaryHigh < 3900) {
+            $qSalaryHigh = 4;
+        } elseif ($qSalaryHigh < 4900) {
+            $qSalaryHigh = 5;
+        } elseif ($qSalaryHigh < 5900) {
+            $qSalaryHigh = 6;
+        } elseif ($qSalaryHigh < 6900) {
+            $qSalaryHigh = 7;
+        } elseif ($qSalaryHigh < 7900) {
+            $qSalaryHigh = 8;
+        } elseif ($qSalaryHigh < 8900) {
+            $qSalaryHigh = 9;
+        } elseif ($qSalaryHigh < 9900) {
+            $qSalaryHigh = 10;
+        } elseif ($qSalaryHigh < 11900) {
+            $qSalaryHigh = 11;
+        } elseif ($qSalaryHigh < 14900) {
+            $qSalaryHigh = 12;
+        } elseif ($qSalaryHigh < 19900) {
+            $qSalaryHigh = 13;
+        } elseif ($qSalaryHigh < 29900) {
+            $qSalaryHigh = 14;
+        } elseif ($qSalaryHigh <= 30000) {
+            $qSalaryHigh = 15;
+        }   
+        
+        $qBornYoung        = date('Y') - (int) Input::get('qAgeYoung');
+        $qBornOld          = date('Y') - (int) Input::get('qAgeOld');
+        
+        $qAcademicLow      = (int) Input::get('qAcademicLow');
+        $qAcademicHigh     = (int) Input::get('qAcademicHigh');
 
-		$qSex              = Input::get('qSex');
-		$qNationalityStr   = Input::get('qNationality');
-		$qNationalityArr   = explode(',', $qNationalityStr);
+        $qSex              = Input::get('qSex');
+        $qNationalityStr   = Input::get('qNationality');
+        $qNationalityArr   = explode(',', $qNationalityStr);
 
-		$qDistrictStr      = Input::get('qDistrict');
-		$qDistrictArr      = explode(',', $qDistrictStr);
+        $qDistrictStr      = Input::get('qDistrict');
+        $qDistrictArr      = explode(',', $qDistrictStr);
 
-		$users = DB::table('users')
-					->where('height',         '>=',  $qHeightSmaller)
-					->where('height',         '<=',  $qHeightTaller)
-					->where('born',           '>=',  $qBornOld)
-					->where('born',           '<=',  $qBornYoung)
-					->where('salary',         '>=',  $qSalaryLow)
-					->where('salary',         '<=',  $qSalaryHigh)
-					->where('academic',       '>=',  $qAcademicLow)
-					->where('academic',       '<=',  $qAcademicHigh)
-					->where('sex',            '=',   $qSex)
-					->where_in('nationality', $qNationalityArr)
-					->where_in('district',    $qDistrictArr)
-					->order_by('id',          'desc')
-					->get();
-					//->get(array('id', 'nickname'));
-					//->get('id');
+        $users = DB::table('users')
+                    ->where('height',         '>=',  $qHeightSmaller)
+                    ->where('height',         '<=',  $qHeightTaller)
+                    ->where('born',           '>=',  $qBornOld)
+                    ->where('born',           '<=',  $qBornYoung)
+                    ->where('salary',         '>=',  $qSalaryLow)
+                    ->where('salary',         '<=',  $qSalaryHigh)
+                    ->where('academic',       '>=',  $qAcademicLow)
+                    ->where('academic',       '<=',  $qAcademicHigh)
+                    ->where('sex',            '=',   $qSex)
+                    ->where_in('nationality', $qNationalityArr)
+                    ->where_in('district',    $qDistrictArr)
+                    ->order_by('id',          'desc')
+                    ->get();
+                    //->get(array('id', 'nickname'));
+                    //->get('id');
 
-		return Response::json($users);
-	}
+        return Response::json($users);
+    }
 
-	public function get_profile($id) {
+    public function get_profile($id) {
 
-		// 写进model里
-	    $user = DB::table('users')
-	    	->join('static_ethnics', 'users.ethnic', '=', 'static_ethnics.id')
-	    	->join('static_marriages', 'users.marriage', '=', 'static_marriages.id')
-	    	->join('static_livings', 'users.living', '=', 'static_livings.id')
-	    	->join('static_districts', 'users.district', '=', 'static_districts.id')
-	    	->join('static_industries', 'users.industry', '=', 'static_industries.id')
-	    	->join('static_companytypes', 'users.companytype', '=', 'static_companytypes.id')
-	    	->join('static_academics', 'users.academic', '=', 'static_academics.id')
-	    	->join('static_nationalities', 'users.nationality', '=', 'static_nationalities.id')
-	    	->join('static_professions', 'users.profession', '=', 'static_professions.id')
-	    	->join('static_salaries', 'users.salary', '=', 'static_salaries.id')
-	    	->where('users.id', '=', $id)
-	    	->first(array(
-	    		'users.id',
-	    		'users.nickname',
-	    		'users.sex',
-	    		'users.height',
-	    		'users.weight',
-	    		'users.born',
-	    		'users.school',
-	    		'users.major',
-	    		'users.blog',
-	    		'static_ethnics.name as ethnic',
-	    		'static_marriages.status as marriage',
-	    		'static_livings.status as living',
-	    		'static_districts.district as district',
-	    		'static_industries.type as industry',
-	    		'static_companytypes.type as companytype',
-	    		'static_academics.academic as academic',
-	    		'static_nationalities.nationality as nationality',
-	    		'static_professions.profession as profession',
-	    		'static_salaries.range as salary'
-	    		)
-	    	);
+        // 写进model里
+        $user = DB::table('users')
+            ->join('static_ethnics', 'users.ethnic', '=', 'static_ethnics.id')
+            ->join('static_marriages', 'users.marriage', '=', 'static_marriages.id')
+            ->join('static_livings', 'users.living', '=', 'static_livings.id')
+            ->join('static_districts', 'users.district', '=', 'static_districts.id')
+            ->join('static_industries', 'users.industry', '=', 'static_industries.id')
+            ->join('static_companytypes', 'users.companytype', '=', 'static_companytypes.id')
+            ->join('static_academics', 'users.academic', '=', 'static_academics.id')
+            ->join('static_nationalities', 'users.nationality', '=', 'static_nationalities.id')
+            ->join('static_professions', 'users.profession', '=', 'static_professions.id')
+            ->join('static_salaries', 'users.salary', '=', 'static_salaries.id')
+            ->where('users.id', '=', $id)
+            ->first(array(
+                'users.id',
+                'users.nickname',
+                'users.sex',
+                'users.height',
+                'users.weight',
+                'users.born',
+                'users.school',
+                'users.major',
+                'users.blog',
+                'static_ethnics.name as ethnic',
+                'static_marriages.status as marriage',
+                'static_livings.status as living',
+                'static_districts.district as district',
+                'static_industries.type as industry',
+                'static_companytypes.type as companytype',
+                'static_academics.academic as academic',
+                'static_nationalities.nationality as nationality',
+                'static_professions.profession as profession',
+                'static_salaries.range as salary'
+                )
+            );
 
-		return View::make('profile')
-			->with('user', $user);
-	}
+        return View::make('profile')
+            ->with('user', $user);
+    }
 }
--- application/routes.php
+++ application/routes.php
@@ -2,89 +2,89 @@
 
 /*
 访问url设计
-	GET http://domain.com/ - 首页 done
-	GET http://domain.com/faq/ - 常见问题解答
-	GET http://domain.com/contact/ - 联系我们
-	GET http://domain.com/privacy/ - 隐私声明
-	GET http://domain.com/help/ - 帮助中心
+    GET http://domain.com/ - 首页 done
+    GET http://domain.com/faq/ - 常见问题解答
+    GET http://domain.com/contact/ - 联系我们
+    GET http://domain.com/privacy/ - 隐私声明
+    GET http://domain.com/help/ - 帮助中心
 
-	GET http://domain.com/dashboard/ - 普通用户控制面板
-	GET http://domain.com/dashboard/stats/ - 普通用户统计信息
+    GET http://domain.com/dashboard/ - 普通用户控制面板
+    GET http://domain.com/dashboard/stats/ - 普通用户统计信息
 
-	GET http://domain.com/admin/dashboard/ - 管理员控制面板
-	GET http://domain.com/admin/dashboard/profile/ - 管理员个人信息
-	GET http://domain.com/admin/manage/ - 管理员登陆
+    GET http://domain.com/admin/dashboard/ - 管理员控制面板
+    GET http://domain.com/admin/dashboard/profile/ - 管理员个人信息
+    GET http://domain.com/admin/manage/ - 管理员登陆
 
 
 */
 
 //homepage
 Route::get('/', array('as' => 'home', function(){
-	return View::make('home.index');
+    return View::make('home.index');
 }));
 
 //common pages
-Route::get('register', 					array('as' => 'register', 										'uses' => 'account@register'));
-Route::post('create', 					array('as' => 'create',											'uses' => 'account@create'));
-Route::put('update', 					array('as' => 'update', 				'before' => 'csrf', 	'uses' => 'account@update'));
-Route::get('verifymail', 				array('as' => 'verifymail',										'uses' => 'account@verifymail'));
-Route::get('verify/(:all)',				array('as' => 'verify', 										'uses' => 'account@verify'));
-Route::get('welcome',					array('as' => 'welcome', 										'uses' => 'account@welcome'));
-Route::get('login', 					array('as' => 'login', 											'uses' => 'account@login'));
-Route::post('login', 					array('as' => 'login', 											'uses' => 'account@login'));
-Route::get('logout', 					array('as' => 'logout', 										'uses' => 'account@logout'));
+Route::get('register',                   array('as' => 'register',                                                 'uses' => 'account@register'));
+Route::post('create',                   array('as' => 'create',                                                    'uses' => 'account@create'));
+Route::put('update',                    array('as' => 'update',                 'before' => 'csrf',     'uses' => 'account@update'));
+Route::get('verifymail',                array('as' => 'verifymail',                                              'uses' => 'account@verifymail'));
+Route::get('verify/(:all)',               array('as' => 'verify',                                                      'uses' => 'account@verify'));
+Route::get('welcome',                array('as' => 'welcome',                                              'uses' => 'account@welcome'));
+Route::get('login',                        array('as' => 'login',                                                     'uses' => 'account@login'));
+Route::post('login',                      array('as' => 'login',                                                     'uses' => 'account@login'));
+Route::get('logout',                      array('as' => 'logout',                                                  'uses' => 'account@logout'));
 
 //function pages
-Route::get('search', 					array('as' => 'search', 				'before' => 'auth', 	'uses' => 'user@search'));
-Route::post('search', 					array('as' => 'search', 										'uses' => 'user@search'));
-Route::get('profile/(:any)', 				array('as' => 'profile', 										'uses' => 'user@profile'));
+Route::get('search',                    array('as' => 'search',                 'before' => 'auth',     'uses' => 'user@search'));
+Route::post('search',                  array('as' => 'search',                                                   'uses' => 'user@search'));
+Route::get('profile/(:any)',           array('as' => 'profile',                                                    'uses' => 'user@profile'));
 
 //site pages
-Route::get('memorabilia', 				array('as' => 'memorabilia', 									'uses' => 'site@memorabilia'));
-Route::get('faq',					array('as' => 'faq', 										'uses' => 'site@faq'));
+Route::get('memorabilia',          array('as' => 'memorabilia',                                        'uses' => 'site@memorabilia'));
+Route::get('faq',                            array('as' => 'faq',                                                         'uses' => 'site@faq'));
 
 //user dashboard
-Route::get('dashboard', 				array('as' => 'dashboard', 										'uses' => 'dashboard@index'));
-Route::get('dashboard/profile', 			array('as' => 'dashboard_profile',								'uses' => 'dashboard@profile'));
-Route::get('dashboard/image', 			array('as' => 'dashboard_image', 								'uses' => 'dashboard@image'));
-Route::post('dashboard/imageupload',		array('as' => 'dashboard_imageupload', 							'uses' => 'dashboard@imageupload'));
-Route::post('dashboard/imagehandle', 		array('as' => 'dashboard_imagehandle',							'uses' => 'dashboard@imagehandle'));
-Route::post('dashboard/imagecrop', 			array('as' => 'dashboard_imagecrop',							'uses' => 'dashboard@imagecrop'));
-Route::delete('dashboard/imagedelete', 		array('as' => 'dashboard_imagedelete', 							'uses' => 'dashboard@imagedelete'));
+Route::get('dashboard',                                      array('as' => 'dashboard',                                      'uses' => 'dashboard@index'));
+Route::get('dashboard/profile',                          array('as' => 'dashboard_profile',                              'uses' => 'dashboard@profile'));
+Route::get('dashboard/image',                         array('as' => 'dashboard_image',                                'uses' => 'dashboard@image'));
+Route::post('dashboard/imageupload',          array('as' => 'dashboard_imageupload',                          'uses' => 'dashboard@imageupload'));
+Route::post('dashboard/imagehandle',          array('as' => 'dashboard_imagehandle',                          'uses' => 'dashboard@imagehandle'));
+Route::post('dashboard/imagecrop',               array('as' => 'dashboard_imagecrop',                            'uses' => 'dashboard@imagecrop'));
+Route::delete('dashboard/imagedelete',        array('as' => 'dashboard_imagedelete',                          'uses' => 'dashboard@imagedelete'));
 
 //admin dashboard
-Route::get('admin/login', 				array('as' => 'admin_login', 									'uses' => 'admin.admin@login'));
-Route::post('admin/login', 				array('as' => 'admin_login', 									'uses' => 'admin.admin@login'));
-Route::get('admin/logout', 				array('as' => 'admin_logout', 									'uses' => 'admin.admin@logout'));
-Route::get('admin/dashboard', 			array('as' => 'admin_dashboard', 								'uses' => 'admin.dashboard@index'));                                   
-Route::get('admin/dashboard/examine', 		array('as' => 'admin_dashboard_examine', 						'uses' => 'admin.dashboard@examine'));                               
+Route::get('admin/login',                                    array('as' => 'admin_login',                                    'uses' => 'admin.admin@login'));
+Route::post('admin/login',                                  array('as' => 'admin_login',                                    'uses' => 'admin.admin@login'));
+Route::get('admin/logout',                                  array('as' => 'admin_logout',                                   'uses' => 'admin.admin@logout'));
+Route::get('admin/dashboard',                         array('as' => 'admin_dashboard',                                'uses' => 'admin.dashboard@index'));                                   
+Route::get('admin/dashboard/examine',        array('as' => 'admin_dashboard_examine',                        'uses' => 'admin.dashboard@examine'));                               
 
 //test pages
 Route::controller(array(
-	'test'
+    'test'
 ));
 
 
 /*
 Route::controller(array(
-	'account',
-	'dashboard'
+    'account',
+    'dashboard'
 ));
 */
 
 
 View::composer(array('layouts.default', 'layouts.dashboard', 'admin.panelindex'), function()
 {
-	//CSS
-	Asset::add('css-bootstrap', 'css/bootstrap.min.css');
-	Asset::add('css-bootstrap-responsive', 'css/bootstrap-responsive.min.css');
-	Asset::add('css-global', 'css/global.css');
+    //CSS
+    Asset::add('css-bootstrap', 'css/bootstrap.min.css');
+    Asset::add('css-bootstrap-responsive', 'css/bootstrap-responsive.min.css');
+    Asset::add('css-global', 'css/global.css');
 
-	//JavaScript
-	Asset::add('jquery', 'js/jquery-1.8.0.min.js');
-	Asset::add('js-bootstrap', 'js/bootstrap.min.js');
-	Asset::add('js-modernizr', 'js/modernizr.custom.js');
-	Asset::add('js-common', 'js/common.js');
+    //JavaScript
+    Asset::add('jquery', 'js/jquery-1.8.0.min.js');
+    Asset::add('js-bootstrap', 'js/bootstrap.min.js');
+    Asset::add('js-modernizr', 'js/modernizr.custom.js');
+    Asset::add('js-common', 'js/common.js');
 });
 
 
@@ -116,12 +116,12 @@ Route::delete('author/delete', array('before' => 'csrf', 'uses' => 'authors@dest
 
 Event::listen('404', function()
 {
-	return Response::error('404');
+    return Response::error('404');
 });
 
 Event::listen('500', function()
 {
-	return Response::error('500');
+    return Response::error('500');
 });
 
 /*
@@ -137,44 +137,44 @@ Event::listen('500', function()
 |
 | 首先，定义一个过滤器：
 |
-|		Route::filter('filter', function()
-|		{
-|			return 'Filtered!';
-|		});
+|       Route::filter('filter', function()
+|       {
+|           return 'Filtered!';
+|       });
 |
 | 然后，把过滤器附加给路由：
 |
-|		Router::register('GET /', array('before' => 'filter', function()
-|		{
-|			return 'Hello World!';
-|		}));
+|       Router::register('GET /', array('before' => 'filter', function()
+|       {
+|           return 'Hello World!';
+|       }));
 |
 */
 
 Route::filter('before', function()
 {
-	// Do stuff before every request to your application...
+    // Do stuff before every request to your application...
 });
 
 Route::filter('after', function($response)
 {
-	// Do stuff after every request to your application...
+    // Do stuff after every request to your application...
 });
 
 Route::filter('csrf', function()
 {
-	if (Request::forged()) return Response::error('500');
+    if (Request::forged()) return Response::error('500');
 });
 
 Route::filter('auth', function()
 {
-	if (Auth::guest()) return Redirect::to('login');
+    if (Auth::guest()) return Redirect::to('login');
 });
 
 // TODO
 Route::filter('auth_admin', function()
 {
-	return 'auth_admin';
+    return 'auth_admin';
 });
 
 
