12b53f4a38248c9755173e7a5cec0bc0a8a080f4
phoenixg-compaq321 <306761352@qq.com>
2013-01-04 21:29:40 +0800

    jiji

--- application/views/dashboard/profile.blade.php
+++ application/views/dashboard/profile.blade.php
@@ -1,7 +1,7 @@
 @layout('layouts.dashboard')
 @section('main')
 	<h2>个人资料编辑</h2>
-	[{{ HTML::link_to_route('profile', '预览个人主页', array(Auth::user()->id)) }}]
+	<span class="preview">[&nbsp;{{ HTML::link_to_route('profile', '预览个人主页', array(Auth::user()->id)) }}&nbsp;]</span>
 
 	@if($errors->has())
 	<div class="alert alert-block alert-error fade in">
