c7298bd527bcdf093bd63860aee2c0b926e3b942
phoenix <306761352@qq.com>
2013-01-04 12:28:38 +0800

    222

--- application/views/layouts/dashboard.blade.php
+++ application/views/layouts/dashboard.blade.php
@@ -56,6 +56,8 @@
                                     资料
                                     @if($verified_status == 1)aa<span class="label label-important nav-status">等待审核</span>@endif
                                     @if($verified_status == 2)bb<span class="label label-success nav-status">已审核</span>@endif
+                                    @if($verified_status == 1)cc@endif
+                                    @if($verified_status == 2)dd@endif
                                 </a>
                             </li>
                             <li class="@if(isset($menuflg_image))active@endif">
